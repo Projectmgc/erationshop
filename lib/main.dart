a0b0dea188ac87fd1388ee6fe89c2158140503f4
Projectmgc <erationprojectmgc@gmail.com>
2024-12-24 16:39:27 +0530

    thjtj

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ Future<void> main() async {
 
   card_no =prefs.getString('card_no');
 
-  runApp(MaterialApp(debugShowCheckedModeBanner: false,home: UhomeScreen()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,home: OwnerHomeScreen()));
 
 
   
